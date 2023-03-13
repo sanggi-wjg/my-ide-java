538eea1ba0c92a08ed78d633d9d0fa7a1fe94b6a
Raynor <jonggu.woo@fitpet.co.kr>
2023-03-13 16:15:45 +0900

    🩹  p6spy formatter 변경

--- src/main/java/com/example/myidejava/core/spy/P6SpyFormatter.java
+++ src/main/java/com/example/myidejava/core/spy/P6SpyFormatter.java
@@ -1,69 +1,73 @@
-//package com.example.myidejava.core.spy;
-//
-//import com.p6spy.engine.logging.Category;
-//import com.p6spy.engine.spy.P6SpyOptions;
-//import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
-//import jakarta.annotation.PostConstruct;
-//import org.hibernate.engine.jdbc.internal.FormatStyle;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.Locale;
-//import java.util.Stack;
-//
-//@Configuration
-//public class P6SpyFormatter implements MessageFormattingStrategy {
-//
-//    @PostConstruct
-//    public void setMessageFormatter() {
-//        P6SpyOptions.getActiveInstance().setLogMessageFormat(this.getClass().getName());
-//    }
-//
-//    @Override
-//    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
-//        sql = formatSql(category, sql);
-//        if (sql == null || sql.trim().isEmpty()) {
-//            return "";
-//        }
+package com.example.myidejava.core.spy;
+
+import com.p6spy.engine.logging.Category;
+import com.p6spy.engine.spy.P6SpyOptions;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import jakarta.annotation.PostConstruct;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Locale;
+import java.util.Stack;
+
+@Configuration
+public class P6SpyFormatter implements MessageFormattingStrategy {
+
+    @PostConstruct
+    public void setMessageFormatter() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(this.getClass().getName());
+    }
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        sql = formatSql(category, sql);
+        if (sql == null || sql.trim().isEmpty()) {
+            return "";
+        }
+        return new StringBuilder()
+                .append("\n( ").append(elapsed).append(" ms)")
+                .append("\tConnection Id: ").append(connectionId)
+                .append(sql).toString();
 //        return sql + createStack(connectionId, elapsed);
-//    }
-//
-//    private String formatSql(String category, String sql) {
-//        if (sql == null || sql.trim().equals("")) return sql;
-//
-//        // Only format Statement, distinguish DDL And DML
-//        if (Category.STATEMENT.getName().equals(category)) {
-//            String tmpsql = sql.trim().toLowerCase(Locale.ROOT);
-//
-//            if (tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")) {
-//                sql = FormatStyle.DDL.getFormatter().format(sql);
-//            } else {
-//                sql = FormatStyle.BASIC.getFormatter().format(sql);
-//            }
-//        }
-//        return sql;
-//    }
-//
-//    private String createStack(int connectionId, long elapsed) {
-//        Stack<String> callStack = new Stack<>();
-//        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-//
-//        for (StackTraceElement stackTraceElement : stackTrace) {
-//            String trace = stackTraceElement.toString();
-//
-//            if (trace.startsWith("com.example.myidejava") && !trace.startsWith("com.example.myidejava.core.spy.P6SpyFormatter")) {
-//                callStack.push(trace);
-//            }
-//        }
-//
-//        StringBuilder sb = new StringBuilder();
-//        int order = 1;
-//        while (!callStack.isEmpty()) {
-//            sb.append("\n\t\t").append(order++).append(". ").append(callStack.pop());
-//        }
-//
-//        return new StringBuilder().append("\n\tConnection ID:").append(connectionId)
-//                .append(" | Execution Time: ").append(elapsed).append(" ms")
-//                .append("\t | Call Stack: ").append(sb).append("\n").toString();
-//    }
-//
-//}
+    }
+
+    private String formatSql(String category, String sql) {
+        if (sql == null || sql.trim().equals("")) return sql;
+
+        // Only format Statement, distinguish DDL And DML
+        if (Category.STATEMENT.getName().equals(category)) {
+            String tmpsql = sql.trim().toLowerCase(Locale.ROOT);
+
+            if (tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")) {
+                sql = FormatStyle.DDL.getFormatter().format(sql);
+            } else {
+                sql = FormatStyle.BASIC.getFormatter().format(sql);
+            }
+        }
+        return sql;
+    }
+
+    private String createStack(int connectionId, long elapsed) {
+        Stack<String> callStack = new Stack<>();
+        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            String trace = stackTraceElement.toString();
+
+            if (trace.startsWith("com.example.myidejava") && !trace.startsWith("com.example.myidejava.core.spy.P6SpyFormatter")) {
+                callStack.push(trace);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int order = 1;
+        while (!callStack.isEmpty()) {
+            sb.append("\n\t\t").append(order++).append(". ").append(callStack.pop());
+        }
+
+        return new StringBuilder().append("\n\tConnection ID:").append(connectionId)
+                .append(" | Execution Time: ").append(elapsed).append(" ms")
+                .append("\t | Call Stack: ").append(sb).append("\n").toString();
+    }
+
+}
